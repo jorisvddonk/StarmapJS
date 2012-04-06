21ae16565179b8e183a895e9ed771c064a666f1c
Joris van de Donk <jorisvddonk@gmail.com>
2012-04-06 22:38:49 +0200

    Merged the stars/constellations data with the planet data. Now everything is structured nicely, and things are quick because everything is precomputed! Yay.

---
+++ starmap-colormapping.js
@@ -0,0 +1,52 @@
+//TODO: Do this in CSS?
+var colormapping =  
+[    
+    {
+        name: "Earthling",
+        color: "#f05015"
+    },
+    {
+        name: "Chenjesu",
+        color: "#6090e0"
+    },    
+    {
+        name: "Mmrnmhrm",
+        color: "#e0e0e0"
+    },
+    {
+        name: "Ilwrath",
+        color: "#E00040"
+    },
+    {
+        name: "Spathi",
+        color: "#f0f000"
+    },
+    {
+        name: "Androsynth",
+        color: "#e000f0"
+    },
+    {
+        name: "Shofixti",
+        color: "#e07038"
+    },
+    {
+        name: "Vux",
+        color: "#00f060"
+    },
+    {
+        name: "Yehat",
+        color: "#f0c010"
+    },
+    {
+        name: "Mycon",
+        color: "#c0a810"
+    },
+    {
+        name: "Umgah",
+        color: "#30d0a0"
+    },
+    {
+        name: "Unknown",
+        color: "#80b050"
+    }
+];
---
+++ starmap-spheres.js
@@ -0,0 +1,209 @@
+var spheres = {
+    none: [],
+    newSpheres:  [
+    {
+        name: "Ilwrath",
+        x: 4.8,
+        y: 170.0,
+        size: 141.0
+    },
+    {
+        name: "Arilou",
+        x: 43.8,
+        y: 637.2,
+        size: 25.0
+    },
+    {
+        name: "Kohr-Ah",
+        x: 600.0,
+        y: 625.0,
+        size: 266.6
+    },
+    {
+        name: "Druuge",
+        x: 950.0,
+        y: 279.2,
+        size: 140.0
+    },
+    {
+        name: "Mycon",
+        x: 639.2,
+        y: 220.0,
+        size: 107.0
+    },
+    {
+        name: "Orz",
+        x: 360.8,
+        y: 263.7,
+        size: 33.3
+    },
+    {
+        name: "Pkunk",
+        x: 50.2,
+        y: 40.1,
+        size: 66.6
+    },
+    {
+        name: "Spathi",
+        x: 254.9,
+        y: 360.0,
+        size: 100.0
+    },
+    {
+        name: "Supox",
+        x: 746.8,
+        y: 924.6,
+        size: 33.3
+    },
+    {
+        name: "Thraddash",
+        x: 253.5,
+        y: 835.8,
+        size: 83.3
+    },
+    {
+        name: "Umgah",
+        x: 179.8,
+        y: 600.0,
+        size: 83.3
+    },
+    {
+        name: "Ur-Quan",
+        x: 575.0,
+        y: 600.0,
+        size: 266.6
+    },
+    {
+        name: "Utwig",
+        x: 853.4,
+        y: 879.7,
+        size: 66.6
+    },
+    {
+        name: "Vux",
+        x: 441.2,
+        y: 155.8,
+        size: 90.0
+    },
+    {
+        name: "Yehat",
+        x: 497.0,
+        y: 4.0,
+        size: 75.0
+    },
+    {
+        name: "Zoq-Fot-Pik",
+        x: 376.1,
+        y: 533.3,
+        size: 32.0
+    }    
+    ],
+
+    oldSpheres: [
+    {
+        name: "Earthling",
+        x: 177.0,
+        y: 144.5,
+        size: 44.0
+    },
+    {
+        name: "Chenjesu",
+        x: 70.0,
+        y: 218.0,
+        size: 64.0
+    },
+    {
+        name: "Mmrnmhrm",
+        x: 68.0,
+        y: 294.0,
+        size: 48.0
+    },
+    {
+        name: "Ilwrath",
+        x: -10.0,
+        y: 355.0,
+        size: 53.0
+    },
+    {
+        name: "Spathi",
+        x: 245.0,
+        y: 365.0,
+        size: 100.0
+    },
+    {
+        name: "Androsynth",
+        x: 370.0,
+        y: 261.0,
+        size: 49.0
+    },
+    {
+        name: "Shofixti",
+        x: 288.0,
+        y: 20.0,
+        size: 37.0
+    },
+    {
+        name: "Vux",
+        x: 431.0,
+        y: 153.0,
+        size: 58.0
+    },
+    {
+        name: "Yehat",
+        x: 495.0,
+        y: 5.0,
+        size: 105.0
+    },
+    {
+        name: "Mycon",
+        x: 624.0,
+        y: 235.0,
+        size: 56.0
+    },
+    {
+        name: "Umgah",
+        x: 185.0,
+        y: 608.0,
+        size: 49.0
+    },
+    {
+        name: "Unknown",
+        x: 58.0,
+        y: 46.0,
+        size: 35.0
+    },
+    {
+        name: "Unknown",
+        x: 945.0,
+        y: 275.0,
+        size: 74.0
+    },
+    {
+        name: "Unknown",
+        x: 282.0,
+        y: 850.0,
+        size: 73.0
+    },
+    {
+        name: "",
+        color: "",
+        x: 0,
+        y: 0,
+        size: 0
+    },
+    {
+        name: "",
+        color: "",
+        x: 0,
+        y: 0,
+        size: 0
+    },
+    {
+        name: "",
+        color: "",
+        x: 0,
+        y: 0,
+        size: 0
+    },
+    ]
+};
