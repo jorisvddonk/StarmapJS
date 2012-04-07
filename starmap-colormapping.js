c33e199ab9622d5d86981d1b7a5489fd9997979a
Joris van de Donk <jorisvddonk@gmail.com>
2012-04-07 19:20:09 +0200

    Display legends when colorizing stars (biological units / mineral value)

--- starmap-colormapping.js
+++ starmap-colormapping.js
@@ -1,4 +1,65 @@
 //TODO: Do this in CSS?
+
+var mindata_mapping = [
+    {
+        min: 0,
+        max: 499,
+        color: "#00CEFF"
+    },
+    {
+        min: 500,
+        max: 999,
+        color: "#00FF00"
+    },
+    {
+        min: 1000,
+        max: 1999,
+        color: "#FFFF00"
+    },
+    {
+        min: 2000,
+        max: 3999,
+        color: "#FF9C00"
+    },
+    {
+        min: 4000,
+        max: 15000,
+        color: "#FF0000"
+    }
+]
+var biodata_mapping = [
+    {
+        min: 0,
+        max: 0,
+        color: "#CCCCCC"
+    },
+    {
+        min: 1,
+        max: 49,
+        color: "#00CEFF"
+    },
+    {
+        min: 50,
+        max: 99,
+        color: "#00FF00"
+    },
+    {
+        min: 100,
+        max: 199,
+        color: "#FFFF00"
+    },
+    {
+        min: 200,
+        max: 299,
+        color: "#FF9C00"
+    },
+    {
+        min: 300,
+        max: 399,
+        color: "#FF0000"
+    }
+]
+
 var colormapping =  
 [    
 {
